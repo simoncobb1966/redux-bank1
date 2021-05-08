f4fea4d4374bdf63ab67d8edd4b4a30c3b80aa19
simoncobb1966 <48588635+simoncobb1966@users.noreply.github.com>
2021-05-08 15:44:58 +0100

    refinements

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import React, { useState } from 'react';
 import "./App.css";
 import { useSelector, useDispatch } from "react-redux";
 import { bindActionCreators } from "redux";
 import { actionCreators } from "./state/index.js";
 function App() {
+
+  const [name, setName]=useState("");
   const account = useSelector((state) => state.account);
   const customers = useSelector((state) => state.customer);
   const dispatch = useDispatch();
@@ -11,12 +14,16 @@ function App() {
     dispatch
   );
 
+const handleName=(e)=>{
+  setName(e.target.value)
+}
+
   const addCustomer=()=>{
-    newCustomer({name:"ian", id:1})
+    newCustomer({name:name, id:Math.floor(Math.random() * 100)})
   };
 
-  const deleteCustomer=()=>{
-    removeCustomer({name:"ian", id:1})
+  const deleteCustomer=(customer)=>{
+    removeCustomer(customer)
   };
 
   return (
@@ -25,11 +32,15 @@ function App() {
       <button onClick={() => depositMoney(1000)}>Deposit</button>
       <button onClick={() => withdrawMoney(500)}>Withdraw</button>
     {customers.map((customer, index)=>{
-      return (<h3 key={index}>{customer.name} {customer.id}</h3>)
+      return (<h3 key={index}>{customer.name} {customer.id}<button onClick={()=>{deleteCustomer(customer)}}>Remove Customer</button></h3>)
     })}
-   
+   <label>Name</label>
+   <input
+   type="text"
+   value={name}
+   onChange={(e)=>{handleName(e)}}
+   />
     <button onClick={()=>{addCustomer()}}>Add Customer</button>
-    <button onClick={()=>{deleteCustomer()}}>Remove Customer</button>
     </div>
   );
 }
